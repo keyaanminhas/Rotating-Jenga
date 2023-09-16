a60442cf8b102c576ee52916c7b4049a207fb044
keyaanminhas <keyaanminhas@gmail.com>
2023-09-17 00:32:02 +0500

    FINAL CODE

--- jenga/jenga.cpp
+++ jenga/jenga.cpp
@@ -4,17 +4,21 @@
 Jenga::Jenga(mode m, const int m1, const int m2, const int m3en)
 	:	_current_mode(m), _m1(m1), _m2(m2), _m3en(m3en)
 {
-	Jenga_setup();
 }
 
 Jenga::Jenga(const int m1, const int m2, const int m3en)
 	:	_current_mode(MEDIUM), _m1(m1), _m2(m2), _m3en(m3en)
 {
-	Jenga_setup();
 }
 
-void Jenga::Jenga_setup(){
+#ifndef ESP32
+
+void Jenga::Jenga_setup(const int EASYSPEED, const int MEDIUMSPEED, const int HARDSPEED, const int CHANCE){
 	randomSeed(analogRead(0));
+	EASY_SPEED = EASYSPEED;
+	MEDIUM_SPEED = MEDIUMSPEED;
+	HARD_SPEED = HARDSPEED;
+	CHANCES = CHANCE;
 	pinMode(_m1, OUTPUT);
 	pinMode(_m2, OUTPUT);
 	pinMode(_m3en, OUTPUT);
@@ -23,8 +27,9 @@ void Jenga::Jenga_setup(){
 	analogWrite(_m3en, 0);
 }
 
+
 void Jenga::Jenga_loop(){
-	if (random(1,16) == 7){
+	if (random(0,CHANCES) == 0){
 		_toggle = !(_toggle);
 	}
 	if (_current_mode == EASY){
@@ -57,3 +62,55 @@ void Jenga::Jenga_loop(){
 	}
 
 }
+
+#else
+
+void Jenga::Jenga_setup(const int EASYSPEED, const int MEDIUMSPEED, const int HARDSPEED, const int CHANCE){
+	randomSeed(analogRead(0));
+	EASY_SPEED = EASYSPEED;
+	MEDIUM_SPEED = MEDIUMSPEED;
+	HARD_SPEED = HARDSPEED;
+	CHANCES = CHANCE;
+	pinMode(_m1, OUTPUT);
+	pinMode(_m2, OUTPUT);
+	motor.setup(_m3en, 0);
+	digitalWrite(_m1, LOW);
+	digitalWrite(_m2, LOW);
+	motor.write(0);
+}
+
+void Jenga::Jenga_loop(){
+	if (random(0,CHANCES) == 0){
+		_toggle = !(_toggle);
+	}
+	if (_current_mode == EASY){
+		motor.write(EASY_SPEED);
+		digitalWrite(_m1, HIGH);
+	}
+	else if (_current_mode == MEDIUM){
+		motor.write(MEDIUM_SPEED);
+		if (_toggle){
+			digitalWrite(_m1, HIGH);
+			digitalWrite(_m2, LOW);
+		}
+		else{
+			digitalWrite(_m1, LOW);
+			digitalWrite(_m2, HIGH);
+		}
+
+	}
+	else if (_current_mode == HARD){
+		motor.write(HARD_SPEED);
+		if (_toggle){
+			digitalWrite(_m1, HIGH);
+			digitalWrite(_m2, LOW);
+		}
+		else{
+			digitalWrite(_m1, LOW);
+			digitalWrite(_m2, HIGH);
+		}
+
+	}
+
+}
+#endif
--- jenga/jenga.h
+++ jenga/jenga.h
@@ -4,9 +4,11 @@
 
 #include "Arduino.h"
 
-#define EASY_SPEED 40
-#define MEDIUM_SPEED 50
-#define HARD_SPEED 70
+
+
+#ifdef ESP32
+#include "esp32pwm.h"
+#endif
 
 
 enum mode {
@@ -22,13 +24,20 @@ private:
 	const int _m1;
 	const int _m2;
 	const int _m3en;
+	int EASY_SPEED;
+	int MEDIUM_SPEED;
+	int HARD_SPEED;
+	int CHANCES;
 	int _current_speed;
 	bool _toggle = false;
+	#ifdef ESP32
+	esp_pwm motor;
+	#endif
 
 public:
 	Jenga(mode m, const int m1, const int m2, const int m3en);
 	Jenga(const int m1, const int m2, const int m3en);
-	void Jenga_setup();
+	void Jenga_setup(const int EASYSPEED, const int MEDIUMSPEED, const int HARDSPEED, const int CHANCE);
 	void Jenga_loop();
 	void set_mode(mode);
 };
